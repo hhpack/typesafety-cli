f3b154c0683cd53ecd9de25d8f0869d82ab35a55
holyshared <holy.shared.design@gmail.com>
2016-12-25 18:04:56 +0900

    add cmdliner

--- src/typesafety.ml
+++ src/typesafety.ml
@@ -1,2 +1,18 @@
+open Cmdliner
+
+let check () =
+  Typesafety_reporter.print_result_file "output.json"
+
+let check_t = Term.(const check)
+
+let info =
+  let doc = "document message" in
+  let man = [ `S "BUGS"; `P "Email bug reports to <hehey at example.org>."; ] in
+  Term.info "typesafety" ~version:"0.1.0" ~doc ~man
+
+let program_terminate = function
+  | `Error _ -> exit 1
+  | _ -> exit 0
+
 let () =
-  Typesafety_reporter.print_result_file "output.json";;
+  program_terminate (Term.eval (check_t, info))
