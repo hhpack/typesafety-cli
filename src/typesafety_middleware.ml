4f1f4fa0b41aac057614724c74ee3dab5e020a30
holyshared <holy.shared.design@gmail.com>
2016-12-28 13:27:52 +0900

    hhvm install check

--- src/typesafety_middleware.ml
+++ src/typesafety_middleware.ml
@@ -1,15 +1,47 @@
-type error = int * string
+open Process
 
-type ('a, 'b) middleware_result =
-  | Next 'a
-  | Done 'a
-  | Error of 'b
+type middleware_error = int * string
 
-type ('i, 'a, 'b) middleware = 'i -> ('a, 'b) middleware_result
+type ('b, 'c) middleware_result =
+  | Next of 'b
+  | Done of 'c
+  | Error of middleware_error
 
-(* WIP Check whether HHVM is installed *)
-let check_hhvm_installed =
-  true
+type ('a, 'b, 'c) a_middleware = unit -> ('b, 'c) middleware_result
+
+module HHVM_version = struct
+  type hhvm_version = {
+    version: string;
+    compiler: string;
+    repo_schema: string;
+  }
+
+  let check_hhvm_version () =
+    try
+      Ok (String.concat "\n" (Process.read_stdout "hhvm" [|"--version"|]))
+    with _ -> Error "hhvm not installed"
+
+  let parse_hhvm_version output =
+    let regexp = Str.regexp "HipHop VM \\(.+\\)\nCompiler: \\(.+\\)\nRepo schema: \\(.+\\)" in
+    let version s = Str.matched_group 1 s in
+    let compiler s = Str.matched_group 2 s in
+    let repo_schema s = Str.matched_group 3 s in
+    if Str.string_match regexp output 0 then
+      Some {
+        version=(version output);
+        compiler=(compiler output);
+        repo_schema=(repo_schema output);
+      }
+    else
+      None
+
+  let check_hhvm_installed () =
+    match check_hhvm_version () with
+      | Ok v -> Next (parse_hhvm_version v)
+      | Error e -> Error (1, e)
+end
+
+let check_hhvm_installed = HHVM_version.check_hhvm_installed
 
 (* WIP Create .hhconfg if it does not exist *)
 let check_hhconfg =
--- tests/test.ml
+++ tests/test.ml
@@ -2,6 +2,7 @@ open OUnit2
 
 let () =
   run_test_tt_main ("all_tests" >::: [
+    Typesafety_middleware_test.tests;
     Typesafety_reporter_test.tests;
     Source_file_test.tests
   ])
