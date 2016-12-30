090b8bc62cae9ddcd4c1c839b9e1578d2c202c0b
holyshared <holy.shared.design@gmail.com>
2016-12-30 16:12:58 +0900

    add check hhconfig middleware

--- src/typesafety_middleware.ml
+++ src/typesafety_middleware.ml
@@ -44,6 +44,31 @@ end
 
 let check_hhvm_installed = HHVM_version.check_hhvm_installed
 
-(* WIP Create .hhconfg if it does not exist *)
-let check_hhconfg =
-  true
+module HHConfg = struct
+  let config_file = ".hhconfig"
+  let config_path dir = dir ^ config_file
+
+  let hhconfg_exists dir =
+    Sys.file_exists (config_path dir)
+
+  let touch_hhconfig dir =
+    let absolute_path = config_path dir in
+    try
+      close_out (open_out absolute_path);
+      Ok absolute_path
+    with Sys_error e -> Error e
+
+  let create_hhconfg_if_not_exists dir =
+    let absolute_path = config_path dir in
+    if hhconfg_exists dir then
+      Ok absolute_path
+    else
+      touch_hhconfig dir
+
+  let check_hhconfg () =
+    match create_hhconfg_if_not_exists (Sys.getcwd ()) with
+      | Ok v -> Next v
+      | Error e -> Error (2, e)
+end
+
+let check_hhconfg = HHConfg.check_hhconfg
--- tests/typesafety_middleware_test.ml
+++ tests/typesafety_middleware_test.ml
@@ -18,4 +18,44 @@ let test_parse_hhvm_version =
       assert_equal result.repo_schema "8eda451fe80742a18e0e2e8917aa4053c2c1afe7";
     )
 
-let tests = "all_tests" >::: [ test_parse_hhvm_version; ]
+module HHConfigTest = struct
+  open HHConfg
+
+  let temp_dir = Filename.get_temp_dir_name ()
+  let temp_config_file = temp_dir ^ ".hhconfig"
+
+  let unlink_config dir =
+    try
+      Sys.remove (dir ^ config_file)
+    with Sys_error _ -> ()
+
+  let assert_hhconfg =
+    match create_hhconfg_if_not_exists temp_dir with
+      | Ok v -> assert_equal v temp_config_file
+      | Error e -> assert_failure e
+
+  let test_hhconfg_not_exists _ =
+    unlink_config temp_dir;
+    if hhconfg_exists temp_dir then assert_failure "File prerequisite is invalid";
+
+    match create_hhconfg_if_not_exists temp_dir with
+      | Ok v -> assert_equal v temp_config_file
+      | Error e -> assert_failure e
+
+  let test_hhconfg_exists _ =
+    if hhconfg_exists temp_dir then unlink_config temp_dir;
+    match touch_hhconfig temp_dir with
+      | Ok v -> assert_hhconfg
+      | Error e -> assert_failure e
+
+  let all_test_hhconfg =
+    "create_hhconfg_if_not_exists" >::: [
+      ("exists" >:: test_hhconfg_exists);
+      ("not_exists" >:: test_hhconfg_not_exists);
+    ]
+end
+
+let tests = "all_tests" >::: [
+  test_parse_hhvm_version;
+  HHConfigTest.all_test_hhconfg;
+]
