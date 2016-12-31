674e9b57a67921e0fcaf1288fb44c3eb00676047
holyshared <holy.shared.design@gmail.com>
2016-12-31 19:08:15 +0900

    add hh_client warpper

--- src/typesafety_middleware.ml
+++ src/typesafety_middleware.ml
@@ -72,3 +72,31 @@ module HHConfg = struct
 end
 
 let check_hhconfg = HHConfg.check_hhconfg
+
+module HHClient = struct
+  open Process
+  open Process.Exit
+  open Process.Output
+
+  let hhclient_command cmd = run "hh_client" [|cmd|]
+  let hhclient_stderr result = String.concat "\n" result.stderr
+
+  let hhclient_error signal result =
+    let signal_string = Signal.to_string signal in
+    let stderr_output = hhclient_stderr result in
+    signal_string ^ ": " ^ stderr_output
+
+  let hhclient_restart () =
+    let result = hhclient_command "restart" in
+    match result.exit_status with
+      | Exit _ -> Ok (hhclient_stderr result)
+      | Kill signal -> Error (hhclient_error signal result)
+      | Stop signal -> Error (hhclient_error signal result)
+
+  let hhclient_check () =
+    let result = hhclient_command "check" in
+    match result.exit_status with
+      | Exit _ -> Ok (hhclient_stderr result)
+      | Kill signal -> Error (hhclient_error signal result)
+      | Stop signal -> Error (hhclient_error signal result)
+end
