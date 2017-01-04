8f708268f2075d909ffc306145c73bf85f79fac4
holyshared <holy.shared.design@gmail.com>
2017-01-04 15:48:58 +0900

    add middleware chain

--- src/typesafety_middleware.ml
+++ src/typesafety_middleware.ml
@@ -9,6 +9,15 @@ type ('b, 'c) middleware_result =
 
 type ('a, 'b, 'c) middleware = unit -> ('b, 'c) middleware_result
 
+let rec middleware_exec middlewares =
+  match middlewares with
+    | [] -> Ok ()
+    | middleware :: remain_middlewares ->
+      match middleware () with
+        | Next _ -> middleware_exec remain_middlewares
+        | Done _ -> middleware_exec remain_middlewares
+        | Error err -> Error err
+
 module HHVM_version = struct
   type hhvm_version = {
     version: string;
