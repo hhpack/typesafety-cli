c7fac411ff196762cd7cfdb7367481b1a2aae52d
holyshared <holy.shared.design@gmail.com>
2017-07-23 18:29:08 +0900

    fix process module

--- src/process/process.ml
+++ src/process/process.ml
@@ -13,20 +13,24 @@ module type S = sig
   val shell_exec: ?ignore:bool -> string -> (Process_result.t, Process_result.t) result Lwt.t
 end
 
-let rec read_line cp =
-  if Lwt_io.is_busy cp#stdout then
-    read_line cp
-  else
-    Lwt_io.read_line_opt cp#stdout
+let read_line ic =
+  let rec inner_read_line ic =
+    if Lwt_io.is_busy ic then
+      inner_read_line ic
+    else
+      Lwt_io.read_line_opt ic in
+  inner_read_line ic
 
-let read_stdout cp ~f =
-  let rec try_read cp =
+let read_stdout ic ~f =
+  let rec try_read ic =
     let on_consumed = function
-      | Some v -> f v; try_read cp
+      | Some v -> f v; try_read ic
       | None -> Lwt.return_none in
 
-    read_line cp >>= on_consumed in
-  try_read cp
+    read_line ic >>= on_consumed in
+  try_read ic
+
+let ignore_close ic = ignore (Lwt_io.close ic)
 
 let exec ?(ignore=false) cmd =
   let buf = Buffer.create 1024 in
@@ -53,14 +57,16 @@ let exec ?(ignore=false) cmd =
       | WSTOPPED v -> return v in
 
   let rec wait_until_consumed cp =
+    let ic = cp#stdout in
+    Gc.finalise ignore_close ic;
     let on_consumed = function
       | None ->
-        Lwt_io.close cp#stdout
+        Lwt_io.close ic
           >>= (fun _ -> cp#status)
           >>= return_result
       | Some _ -> wait_until_consumed cp in
     match cp#state with
-      | Running -> read_stdout cp ~f:writeln >>= on_consumed
+      | Running -> read_stdout ic ~f:writeln >>= on_consumed
       | Exited status -> return_result status in
 
   wait_until_consumed cp
