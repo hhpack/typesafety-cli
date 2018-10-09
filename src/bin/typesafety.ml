b3914a70dff79cc82e2fe725a7afb70ee6df44b0
holyshared <holy.shared.design@gmail.com>
2018-10-09 11:27:41 +0900

    v0.13.0

--- src/bin/typesafety.ml
+++ src/bin/typesafety.ml
@@ -11,7 +11,7 @@ open Cli
 open Typechecker
 
 let program_name = "typesafety"
-let program_version = "0.12.1"
+let program_version = "0.13.0"
 
 let print_if_typecheck_passed json ~msg =
   let open Typechecker_check_t in
